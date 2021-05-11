26b7ed12b8c00c2e13bbea2173cb62ae8a9f0dde
Kenji Wada <k.wada@Kenji-mbp.local>
2021-05-11 18:09:35 +0200

    deploy check5

--- server.js
+++ server.js
@@ -1,36 +1,16 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + './dist/experiment1-tue'));
-
-// app.get('/*', function(req,res) {
-
-// res.sendFile(path.join(__dirname+'/dist/experiment1-tue/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static(__dirname + './dist/experiment1-tue'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-);
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
