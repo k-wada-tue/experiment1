c9253a8797272de7481d4aa96fd94ccf6505eb79
Kenji Wada <k.wada@Kenji-mbp.local>
2021-05-20 14:46:08 +0200

    console.re test v4

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const path = require('path');
 
 const app = express();
 
+const consolere = require('console-remote-client').connect({
+  server: 'http://localhost:8088',
+  channel: 'experiment1-tue', // required
+  redirectDefaultConsoleToRemote: true, // optional, default: false
+  disableDefaultConsoleOutput: true, // optional, default: false
+});
+
+console.log('test log');
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
@@ -14,4 +23,6 @@ res.sendFile(path.join(__dirname + '/dist/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log('test log node');
 
