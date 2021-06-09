77d36f8a7bafea2a2f70d05a0f3de394766df37f
Kenji Wada <k.wada@Kenji-mbp.local>
2021-06-09 19:09:10 +0200

    succeed with sending click event traking with this version

--- src/app/content/content-right/overlay-data/overlay-data.component.ts
+++ src/app/content/content-right/overlay-data/overlay-data.component.ts
@@ -2,13 +2,23 @@ import { Component} from '@angular/core';
 //import {MatCheckboxModule} from '@angular/material/checkbox';
 
 declare function addOverlay(status, value): any;
-//export function trackOverlay(): any;
-// declare let checkStatus: any;
-// declare let checkValue: any;
+//declare function trackOverlay(): any;
 
 declare let gtag: any;
 export let checkStatus: any;
 export let checkValue: any;
+
+function trackOverlay(value) {
+  if (checkStatus == true) { //send click data only when checkbox is checked (not unchecked)
+    console.log('excuted');
+    console.log(value);
+    gtag('event', 'click', {
+      'event_category': 'checked',
+      'event_label': value,
+      //'event_status': 'checkbox_' + checkStatus,
+      'value': 0 })
+  }
+}
 
 @Component ({
   selector: 'overlay-data',
@@ -21,18 +31,13 @@ export class OverlayDataComponent {
   // checked = true;
 
   onAddOverlay(event) {
-    console.log(event.checked);
-    checkStatus = event.checked;
 
+    checkStatus = event.checked;
+    console.log(checkStatus);
     checkValue = event.source.value;
 
-    addOverlay(event.checked, event.source.value);
-
-    gtag('event', 'click', {
-      'event_category': 'checked',
-      'event_label': 'something',
-      //'event_status': 'checkbox_' + checkStatus,
-      'value': 0 })
+    addOverlay(event.checked, checkValue);
+    trackOverlay(checkValue);
   }
 }
 
